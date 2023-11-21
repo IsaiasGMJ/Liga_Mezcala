43382f7f91faac12ba8b752a71a7ea97d84ab2cc
Isaías Gamaliel Martínez Juárez <135473698+IsaiasGMJ@users.noreply.github.com>
2023-11-21 00:56:52 -0600

    Add files via upload

---
+++ LigaMezcala/Administrador/Dashboard/Admin.php
@@ -0,0 +1,150 @@
+<?php
+/*
+require_once("archivos/UsuariosControlador.php");
+require_once("archivos/ClinicasControlador.php");
+require_once("archivos/DatosMedicosControlador.php");
+require_once("archivos/DatosPersonalesControlador.php");
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Dashboard - Liga Mezcala</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/R.ico" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Bootstrap´Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!--CSS3-->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="assets/img/R.ico" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light"><a href="index.html">Liga Mezcala Admin</a></h1>
+      </div>
+
+      <nav id="navbar" class="nav-menu navbar">
+        <ul>
+        <li><a href="#Inicio" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Inicio</span></a></li>
+          <li><a href="#Equipos" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Equipos</span></a></li>
+          <li><a href="#Jugadores" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Jugadores</span></a></li>        
+          <li><a href="#Estadisticas" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Estadisticas</span></a></li> 
+          <li><a href="#Agenda" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Agenda</span></a></li> 
+        </ul>
+      </nav><!-- .nav-menu -->
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Inicio Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Liga Mezcala</h1>
+      <p>Administra mas <span class="typed" data-typed-items="Facil!, Eficiente!, desde un solo lugar!"></span></p>
+    </div>
+  </section><!-- End inicio -->
+
+  <main id="main">
+
+    <!-- ======= Equipos Section ======= -->
+    <section id="Equipos" class="services">
+      <div class="container">
+        <div class="section-title">
+          <h2>Equipos</h2>
+          <p></p>
+        </div>
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Jugadores Section ======= -->
+    <section id="Jugadores" class="testimonials section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Jugadores</h2>
+          <p></p>
+        </div>
+      </div>
+    </section><!-- End Jugadores Section -->
+
+        <!-- ======= Estadisticas Section ======= -->
+    <section id="Estadisticas" class="services">
+      <div class="container">
+        <div class="section-title">
+          <h2>Estadisticas</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam nesciunt veniam sapiente rem quidem, aut neque vero a 
+            <br><br>
+            <br>accusantium magni fuga illo totam quibusdam eos corrupti similique ea eius quia.</p>
+        </div>
+      </div>
+    </section><!-- End Estadisticas Section -->
+
+          <!-- ======= Agenda Section ======= -->
+    <section id="Agenda" class="testimonials section-bg">
+      <div class="container">
+        <div class="section-title">
+          <h2>Agenda</h2>
+          <p><br>a<br><br><br><br><br></p>
+        </div>
+      </div>
+    </section><!-- End Agenda Section -->
+
+    
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span><a href="/LigaMezcala/Index.php"><i class="Firesoftlink"></i>LigaMezcala</a></span></strong>
+      </div>
+      <div class="credits">
+        Designed by <a href="/../Firesoft">Firesoft</a>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.umd.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
